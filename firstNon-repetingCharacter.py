9272a2dc2d0f36c640206f89132ebf71076c7503
PrakashPrabhu-M <54565583+PrakashPrabhu-M@users.noreply.github.com>
2019-10-07 19:51:30 +0530

    Create firstNon-repetingCharacter.py
    You are given a string ‘S’ consisting of lowercase Latin Letters. Find the first non repeating character in S. If you find all the characters are repeating print the answer as -1
    
    Input Description:
    You are given a string ‘s’
    
    Output Description:
    Print the first non occurring character if possible else -1.
    
    Sample Input :
    apple
    
    Sample Output :
    a

---
+++ firstNon-repetingCharacter.py
@@ -0,0 +1,21 @@
+'''
+You are given a string ‘S’ consisting of lowercase Latin Letters. Find the first non repeating character in S. If you find all the characters are repeating print the answer as -1
+
+Input Description:
+You are given a string ‘s’
+
+Output Description:
+Print the first non occurring character if possible else -1.
+
+Sample Input :
+apple
+
+Sample Output :
+a
+'''
+s=input()
+def func(s):
+    for i in s:
+        if s.count(i)==1:
+            return i
+print(func(s))
