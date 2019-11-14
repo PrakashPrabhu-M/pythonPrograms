267db274c86dc998f1c52c4336e56efb8668464f
PrakashPrabhu-M <54565583+PrakashPrabhu-M@users.noreply.github.com>
2019-11-14 17:20:02 +0530

    Create noOfPairs.py

---
+++ noOfPairs.py
@@ -0,0 +1,46 @@
+'''
+
+
+You are given with a number ‘n’. You have to count the pair of two numbers a and b such that sum of two numbers are equal to n.
+
+ 
+
+Note:Both numbers lie in range 1<=a,b<n
+
+Input Description:
+You are given a number ‘n’
+
+Output Description:
+Print the number of pairs satisfying above condition
+
+Sample Input :
+5
+
+Sample Output :
+4
+
+'''
+print(int(input())-1)
+
+# n=int(input())
+# c=0
+# for i in range(1,n):
+# 	c+=1
+# print(c)
+
+# n=int(input())
+# def pair(n):
+#     c=0
+#     for i in range(1,n):
+#         c+=1
+#     return c
+# print(pair(n))
+
+# n=int(input())
+# def pair(n):
+#     c=0
+#     for i in range(1,n):
+#         if i+n-i==n:
+#             c+=1
+#     return c
+# print(pair(n))
