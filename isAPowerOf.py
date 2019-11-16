349d1853bb0b07f27d5f29cfb7ea3f0904e2ae44
PrakashPrabhu-M <54565583+PrakashPrabhu-M@users.noreply.github.com>
2019-11-16 11:16:48 +0530

    Update isAPowerOf.py

--- isAPowerOf.py
+++ isAPowerOf.py
@@ -5,6 +5,21 @@ INPUT
 OUTPUT
 yes """
 
+'''
 n,k=[int(x) for x in input().split()]
 powers=[k**x for x in range(2,100)]
 print('yes' if n in powers else 'no')
+'''
+
+i=1
+n,k=[int(x) for x in input().split()]
+def isPower(n,k,i):
+    a=k**i
+    if n==a:
+        return True
+    elif a<n:
+        i+=1
+        return isPower(n,k,i)
+    else:
+        return False
+print('yes' if isPower(n,k,i) else 'no')
